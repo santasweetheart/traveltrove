241834838482d0808046acf8ba7b84c90b76f2b1
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-05-30 20:02:41 -0400

    Created App

---
+++ traveltrove/traveltrove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traveltrove
+//
+//  Created by Katherine on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
