aacff7ae399d5014d2fa8e0a20513253e9f64d9c
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-06-04 00:43:35 -0400

    Added App Icon

--- traveltrove/traveltrove/ViewController.swift
+++ traveltrove/traveltrove/ViewController.swift
@@ -13,37 +13,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .customTan
-        createLabel()
-        initConstraints()
-        print("Font being used: \(hello.font!.fontName)")
         
-        for family in UIFont.familyNames {
-            print("Font family: \(family)")
-            for fontName in UIFont.fontNames(forFamilyName: family) {
-                print(" - Font name: \(fontName)")
-            }
-        }
+//        for family in UIFont.familyNames {
+//            print("Font family: \(family)")
+//            for fontName in UIFont.fontNames(forFamilyName: family) {
+//                print(" - Font name: \(fontName)")
+//            }
+//        }
 
-    }
-    
-    
-    func createLabel() {
-        hello = UILabel()
-        hello.text = "Hello World"
-        hello.font = UIFont(name:"Imprima-Regular", size: 16)
-        hello.textAlignment = .center
-        hello.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(hello)
-    }
-    
-    func initConstraints() {
-        // Activate the constraints
-        NSLayoutConstraint.activate([
-            hello.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            hello.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
-        ])
     }
 
 
