e783a5535ab38e8f9f6862a0605c3d1d289b3be3
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-06-03 23:39:45 -0400

    Added Custom Color Palette

---
+++ traveltrove/traveltrove/Custom Elements/ColorPalette.swift
@@ -0,0 +1,23 @@
+//
+//  ColorPalette.swift
+//  traveltrove
+//
+//  Created by Katherine on 6/3/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    static var customPeach : UIColor {
+        return UIColor(red: 255, green: 203, blue: 173, alpha: 1.0)
+    }
+    
+    static var customTan : UIColor {
+        return UIColor(red: 254, green: 237, blue: 218, alpha: 1.0)
+    }
+    
+    static var customGreen : UIColor {
+        return UIColor(red: 169, green: 198, blue: 183, alpha: 1.0)
+    }
+}
