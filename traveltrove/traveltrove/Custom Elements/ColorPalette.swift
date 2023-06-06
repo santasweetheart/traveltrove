9a75d683828c7130e357df4029f9d7942baadba1
Santabarbara Jolicoeur <87780992+santasweetheart@users.noreply.github.com>
2023-06-06 09:34:09 -0400

    11: Added Some Custom Styling (#29)
    * Added Custom Color Palette
    
    * Added Assets
    
    * Fixed Custom Colors
    
    Values were  RGB values were change to be between 0 - 1.
    
    To test them out change the color of the view's background
    
    * All Assets Added
    
    * Trying to Get font to work
    
    * Added App Icon

---
+++ traveltrove/traveltrove/Custom Elements/ColorPalette.swift
@@ -0,0 +1,23 @@
+//
+//  ColorPalette.swift
+//  traveltrove
+//
+//  Created by Katherine on 6/3/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    static var customPeach: UIColor {
+        return UIColor(red: 255/255, green: 203/255, blue: 173/255, alpha: 1.0)
+    }
+
+    static var customTan: UIColor {
+        return UIColor(red: 254/255, green: 237/255, blue: 218/255, alpha: 1.0)
+    }
+
+    static var customGreen: UIColor {
+        return UIColor(red: 169/255, green: 198/255, blue: 183/255, alpha: 1.0)
+    }
+}
--- traveltrove/traveltrove/ViewController.swift
+++ traveltrove/traveltrove/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var hello: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        for family in UIFont.familyNames {
+//            print("Font family: \(family)")
+//            for fontName in UIFont.fontNames(forFamilyName: family) {
+//                print(" - Font name: \(fontName)")
+//            }
+//        }
+
     }
 
 
