fc5d0902d5dd24578d6642fca793edf38bf8bc9f
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-06-06 12:56:02 -0400

    Added more custom colors

--- traveltrove/traveltrove/Custom Elements/ColorPalette.swift
+++ traveltrove/traveltrove/Custom Elements/ColorPalette.swift
@@ -16,6 +16,14 @@ extension UIColor{
     static var customTan: UIColor {
         return UIColor(red: 254/255, green: 237/255, blue: 218/255, alpha: 1.0)
     }
+    
+    static var customDarkPink: UIColor {
+        return UIColor(red: 244/255, green: 180/255, blue: 165/255, alpha: 1.0)
+    }
+    
+    static var customLightPink: UIColor {
+        return UIColor(red: 252/255, green: 195/255, blue: 182/255, alpha: 1.0)
+    }
 
     static var customGreen: UIColor {
         return UIColor(red: 169/255, green: 198/255, blue: 183/255, alpha: 1.0)
---
+++ traveltrove/traveltrove/Landing Page/LandingPageView.swift
@@ -0,0 +1,21 @@
+//
+//  LandingPage.swift
+//  traveltrove
+//
+//  Created by Katherine on 6/6/23.
+//
+
+import UIKit
+
+class LandingPageView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .customPeach
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
