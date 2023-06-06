0bf007464356582728e51a718039aef136e66704
dillian14868 <pica-borjas.d@northeastern.edu>
2023-06-06 20:08:13 +0200

    LoginPage Done

---
+++ traveltrove/traveltrove/LoginPageView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPageView.swift
+//  traveltrove
+//
+//  Created by Dillian Pica on 6/6/23.
+//
+
+import UIKit
+
+class LoginPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
