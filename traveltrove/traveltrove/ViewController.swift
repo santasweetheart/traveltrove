37de96f31a4789fa80e518973df20506e6517c85
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-06-13 16:46:08 -0400

    Added IntroScreen back - avoid merge error

--- traveltrove/traveltrove/ViewController.swift
+++ traveltrove/traveltrove/ViewController.swift
@@ -8,11 +8,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func loadView() {
-    }
+    let introscreen = IntroPageView()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func loadView() {
+        view = introscreen
     }
 }
